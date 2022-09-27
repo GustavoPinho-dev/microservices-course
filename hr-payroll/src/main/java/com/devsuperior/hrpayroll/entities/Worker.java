96246af251e5681c58ff0a96e144b0a1df499e37
godh <gustavo.rezende@gft.com>
2022-09-27 15:05:58 -0300

    RestTemplate

---
+++ hr-payroll/src/main/java/com/devsuperior/hrpayroll/entities/Worker.java
@@ -0,0 +1,41 @@
+package com.devsuperior.hrpayroll.entities;
+
+public class Worker {
+
+    private Long id;
+    private String name;
+    private double dailyIncome;
+
+    public Worker() {
+    }
+
+    public Worker(Long id, String name, double dailyIncome) {
+        this.id = id;
+        this.name = name;
+        this.dailyIncome = dailyIncome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getDailyIncome() {
+        return dailyIncome;
+    }
+
+    public void setDailyIncome(double dailyIncome) {
+        this.dailyIncome = dailyIncome;
+    }
+}
